0037e1974b4788cda98d66761b336a0a037561c1
trave5@yahoo.it <trave5@yahoo.it>
2015-08-13 21:36:59 +0200

    Nuovo rilascio

---
+++ src/main/java/UserProfileActionListener.java
@@ -0,0 +1,17 @@
+package com.javacodegeeks.jsfcomplisteners;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+public class UserProfileActionListener implements ActionListener {
+
+ public void processAction(ActionEvent event)
+  throws AbortProcessingException {
+   UserProfile userProfile = (UserProfile) FacesContext
+    .getCurrentInstance().getExternalContext().getSessionMap()
+    .get("userProfile");
+   userProfile.updateGreeting(event);
+ }
+}
