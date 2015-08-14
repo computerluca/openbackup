a8a3f28f4776610e2a9e45e0fe5f0bbf0ae0dd7c
trave5@yahoo.it <trave5@yahoo.it>
2015-08-14 22:26:37 +0200

    Vaffanculo ai commit

---
+++ src/main/java/Commerciale.java
@@ -0,0 +1,54 @@
+package com.javacodegeeks.jsfcomplisteners;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+public class Commerciale(){
+	public DocumentBuilderFactory builderFactory;
+	public DocumentBuilder builder;
+	public String file;
+	public Document xmlDocument;
+	public XPath xPath;
+ public Commerciale (String file_xml){
+	builderFactory = DocumentBuilderFactory.newInstance();
+	builder = null;
+try {
+    builder = builderFactory.newDocumentBuilder();
+} catch (ParserConfigurationException e) {
+    e.printStackTrace();  
+}
+	this.file=file_xml;
+	try {
+    xmlDocument = builder.parse(new ByteArrayInputStream(file.getBytes()));
+} catch (SAXException e) {
+    e.printStackTrace();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+xpath = XPathFactory.newInstance().newXPath();
+}
+public Double somma_iva_dettagli(){
+ 
+        String prezzo_totale = null;
+        Double prezzo_tot = 0.00;
+
+        String expression2 = "sum(FatturaElettronica/FatturaElettronicaBody/DatiBeniServizi/DettaglioLinee/PrezzoTotale)";
+        try {
+            prezzo_totale = xpath.compile(expression2).evaluate(this.xmlDocument);
+
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(FEPACHECKXML.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        prezzo_tot = Double.parseDouble(prezzo_totale);
+
+        return prezzo_tot;
+}
+public Double somma_iva_riepilogo(){
+	
+}
--- src/main/java/JavaVersion.java
+++ src/main/java/JavaVersion.java
@@ -9,18 +9,27 @@ import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean(name = "javaVersion", eager = true)
 @SessionScoped
+
 public class JavaVersion {
 
  private Double iva;
  private Double somma_iva_dettagli;
+ private String file;
+ public void setfile(String file) {
+  this.file = file;
+ }
 
- public void setiva(Double iva) {
+ public String getfile() {
+  return file;
+ }
+  public void setiva(Double iva) {
   this.iva = iva;
  }
 
  public Double getiva() {
   return iva;
  }
+ 
  public void setsomma_iva_dettagli(Double sid) {
   this.somma_iva_dettagli = 10.0;
  }
@@ -30,6 +39,8 @@ public class JavaVersion {
  }
  public void ivacambiata(ValueChangeEvent event) {
   iva = Double.parseDouble(event.getNewValue().toString());
+  Commerciale comm = new Commerciale(this.file);
+  somma_iva_dettagli= comm.somma_iva_dettagli();
  }
 
 }
