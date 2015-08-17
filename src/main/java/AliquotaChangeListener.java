32f5dd772e3c972482ef63e61b519045d0c00842
trave5@yahoo.it <trave5@yahoo.it>
2015-08-17 21:29:13 +0200

    jlajòldfj

--- src/main/java/AliquotaChangeListener.java
+++ src/main/java/AliquotaChangeListener.java
@@ -1,18 +1,1 @@
-import javax.faces.context.FacesContext;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.ValueChangeEvent;
-import javax.faces.event.ValueChangeListener;
-
-public class AliquotaChangeListener implements ValueChangeListener{
-
- public void processValueChange(ValueChangeEvent event)
-  throws AbortProcessingException {
-   javaVersion javaVersion= (JavaVersion) FacesContext.getCurrentInstance().
-    getExternalContext().getSessionMap().get("javaVersion");
-   javaVersion.setfile(Double.parseDouble(event.getNewValue()));
- 
- }
- 
-
-}
 
