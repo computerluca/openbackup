216b5efb86e83a28aeda3a6610aba89b84b09199
trave5@yahoo.it <trave5@yahoo.it>
2015-08-18 22:07:45 +0200

    Adlfkaj

--- src/main/java/JavaVersionActionListener.java
+++ src/main/java/JavaVersionActionListener.java
@@ -1,10 +1,11 @@
+import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
 
 public class JavaVersionActionListener implements ActionListener{
 
-	 @Override
+         @Override
 	 public void processAction(ActionEvent arg0) 
    throws AbortProcessingException {
       //access userData bean directly
