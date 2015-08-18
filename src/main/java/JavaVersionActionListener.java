606c57b0e1e1fd81113ac3ec4bf8c0b19a86c271
trave5@yahoo.it <trave5@yahoo.it>
2015-08-18 20:55:31 +0200

    Adlfkaj

---
+++ src/main/java/JavaVersionActionListener.java
@@ -0,0 +1,15 @@
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+public class JavaVersionActionListener implements ActionListener{
+
+	@Override
+	public void processAction(ActionEvent event)
+		throws AbortProcessingException {
+		
+		System..out.println("Any use case here?");
+	
+	}
+	
+}
