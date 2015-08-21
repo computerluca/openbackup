d3f8610a379cd66c8cf1aab14579247e71fdd2b1
trave5@yahoo.it <trave5@yahoo.it>
2015-08-21 22:25:06 +0200

    commit

--- src/main/java/Upload.java
+++ src/main/java/Upload.java
@@ -1,32 +1,16 @@
- 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
 import javax.faces.context.FacesContext;
  
-
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
  
 @ManagedBean(name = "upload")
 @SessionScoped
-
 public class Upload {
-     
-    private UploadedFile file;
  
-    public UploadedFile getFile() {
-        return file;
-    }
- 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-     
-    public void upload() {
-        if(file != null) {
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 }
