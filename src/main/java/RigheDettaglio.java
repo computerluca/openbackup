dd63bbdc563fb2eac59cc2e0184fa34007f450b3
trave5@yahoo.it <trave5@yahoo.it>
2015-08-23 00:24:50 +0200

    Quasi primo rilascio stabile della versione web di checkfepa, manca qualche ritocco

--- src/main/java/RigheDettaglio.java
+++ src/main/java/RigheDettaglio.java
@@ -59,7 +59,7 @@ public class RigheDettaglio implements Serializable{
                 Double aliquota = 0.00;
                 aliquota = Double.parseDouble(eElement.getElementsByTagName("AliquotaIVA").item(0).getTextContent().trim());
                 Double importo_iva = 0.00;
-                importo_iva = this.comm.round((prezzo_totale*importo_iva)/100);
+                importo_iva = this.comm.round((prezzo_totale*aliquota)/100);
                 Double totale = 0.00;
                 totale = this.comm.round(prezzo_totale+importo_iva);
                 		 	Order prova = new Order(nrlinea,descrizione,prezzoUnitario,quantita,prezzo_totale,aliquota,importo_iva,totale);
