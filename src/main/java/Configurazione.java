49b021b683e9cf4b0835c270e2e24a6fb626a105
trave5@yahoo.it <trave5@yahoo.it>
2015-09-01 23:32:16 +0200

    merda

--- src/main/java/Configurazione.java
+++ src/main/java/Configurazione.java
@@ -4,6 +4,8 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.event.ValueChangeEvent;
+import javax.faces.bean.*;
+import javax.validation.constraints.*;
 @ManagedBean(name = "configurazione")
 
 @RequestScoped
