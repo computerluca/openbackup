6ce16830919d3cc136738c0dfa2d6199765aba39
trave5@yahoo.it <trave5@yahoo.it>
2015-10-04 10:48:49 +0200

    ciao

--- src/main/java/SaveManager.java
+++ src/main/java/SaveManager.java
@@ -71,6 +71,8 @@ String URL = "jdbc:mysql://127.13.47.2:3306/computerlucaworld";
 Connection conn = null;
 try{
 conn = DriverManager.getConnection(URL , USERNAME , PASSWORD); 
+
+
 PreparedStatement pstmt = conn.prepareStatement("INSERT INTO AC01_ANAGRAFICHE (ID_ANAGRAFICA,ID_CONFIGURAZIONE)VALUES(NULL,1)");
 		        pstmt.executeUpdate();
 
