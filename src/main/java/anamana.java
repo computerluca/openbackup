55f7bacddce4d772cc42c2bcdaae55a67699e223
trave5@yahoo.it <trave5@yahoo.it>
2015-11-28 19:28:01 +0100

    ciao

--- src/main/java/anamana.java
+++ src/main/java/anamana.java
@@ -1,7 +1,7 @@
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
- 
+ import java.util.*;
 @ManagedBean
 @SessionScoped
 public class anamana implements Serializable {
@@ -9,7 +9,7 @@ public class anamana implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public AC01_ANAGRAFICHE_UTILITIES lista_anagrafiche = new AC01_ANAGRAFICHE_UTILITIES();
 	public List lista_anagrafiche_lista;
-	public List inizializza(){
+	public void inizializza(){
 		this.lista_anagrafiche_lista=lista_anagrafiche.select_all_ac01_anagrafiche();
 	}
 	public String newana(){
