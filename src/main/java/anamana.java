98405000afd93daee0133d400d766e958f8d04b6
trave5@yahoo.it <trave5@yahoo.it>
2015-11-28 19:24:54 +0100

    ciao

--- src/main/java/anamana.java
+++ src/main/java/anamana.java
@@ -8,8 +8,10 @@ public class anamana implements Serializable {
  
 	private static final long serialVersionUID = 1L;
 	public AC01_ANAGRAFICHE_UTILITIES lista_anagrafiche = new AC01_ANAGRAFICHE_UTILITIES();
-	public List lista_anagrafiche_lista = lista_anagrafiche.select_all_ac01_anagrafiche();
-	
+	public List lista_anagrafiche_lista;
+	public List inizializza(){
+		this.lista_anagrafiche_lista=lista_anagrafiche.select_all_ac01_anagrafiche();
+	}
 	public String newana(){
 		return "success";
 	}
