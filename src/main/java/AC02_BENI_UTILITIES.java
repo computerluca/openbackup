e832604b7f4fd33ac92c6facc7f5dcccf2d47f8e
trave5@yahoo.it <trave5@yahoo.it>
2016-01-11 19:22:53 +0100

    ciao

--- src/main/java/AC02_BENI_UTILITIES.java
+++ src/main/java/AC02_BENI_UTILITIES.java
@@ -43,8 +43,7 @@ System.out.println(pstmt.toString());
             //per ogni riga nella tabella aggiungi
             //un elemento a una lista
 
-          			car.setIdbene(rs.getInt("id_bene"));
-
+          	car.setId_bene(rs.getInt("id_bene"));
 			car.setcodice_bene(rs.getString("codice_bene"));
 			car.setdescrizione(rs.getString("descrizione"));
 			
