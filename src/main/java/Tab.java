484a8bbfb3fb49b5954f3a0cb7309e01406477f3
trave5@yahoo.it <trave5@yahoo.it>
2016-01-14 23:53:56 +0100

    Framework Tab Dinamici

--- src/main/java/Tab.java
+++ src/main/java/Tab.java
@@ -12,6 +12,30 @@ public class Tab{
 		
 	}
 	
+	public String gettitolo()
+	{
+		return this.titolo;
+	}
+	
+	public void settitolo(String titolo)
+	{
+		this.titolo = titolo;
+		
+	}
+	
+	
+	
+	public String getdescrizione()
+	{
+		return this.descrizione;
+	}
+	
+	public void setdescrizione(String descrizione)
+	{
+		this.descrizione = descrizione;
+		
+	}
+	
 	
 	
 	
