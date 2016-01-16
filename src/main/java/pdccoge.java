21a05d47768188ea3b456041bc7b1370dbec58ab
trave5@yahoo.it <trave5@yahoo.it>
2016-01-16 15:24:38 +0100

    Framework Tab Dinamici

--- src/main/java/pdccoge.java
+++ src/main/java/pdccoge.java
@@ -17,7 +17,7 @@ public class pdccoge implements Serializable {
     public void init() {
 		C002_PDC_COGE_UTILITIES pianodeiconti= new C002_PDC_COGE_UTILITIES();
 		List<C002_PDC_COGE> padre = pianodeiconti.get_pdc_padre();
-		String stringa_padre = padre.get(0).cd_voce_coge.toString();
+		String stringa_padre = padre.get(0).cd_voce_coge;
         root = new DefaultTreeNode("Root",null);
         TreeNode node0 = new DefaultTreeNode(stringa_padre,null,root);
         TreeNode node1 = new DefaultTreeNode("Node 1", root);
