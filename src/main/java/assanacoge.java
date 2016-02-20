1e0c0ee897fab08bc26d6049778211a78239e205
computerlucaworld <trave5@yahoo.it>
2016-02-20 19:58:28 +0100

    cazzo

--- src/main/java/assanacoge.java
+++ src/main/java/assanacoge.java
@@ -25,7 +25,7 @@ public class assanacoge {
 		this.list_ana = anautil.select_all_ac01_anagrafiche();
 		this.list_coge = cogeutil.get_all();
 		for(int i=0;i<this.list_coge.size();i++){
-			System.out.println(this.list_coge.getdenominazione());
+			System.out.println(this.list_coge.getDenominazione());
 		}
 	
 	}
